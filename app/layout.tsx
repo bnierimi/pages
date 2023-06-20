a8874bb58f150cbf2dc185fd8e32a78550d2ae30
bnierimi <shemiradiance@gmail.com>
2023-06-21 00:36:20 +0100

    (bnierimi): added `/styles` and `font` [subject to change]

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,8 @@ import Account from '@/components/Account'
 
 // const inter = Inter({ subsets: ['latin'] })
 
+import { instrumentSans } from '@/styles/fonts'
+
 export const metadata = {
   title: 'Tcitrogg-app',
   description: 'App created by Tcitrogg',
@@ -26,7 +28,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`{inter.className} font-sans bg-zinc-100 dark:bg-zinc-900 text-zinc-700 dark:text-zinc-200 overflow- `}>
+      <body className={`${instrumentSans.className} font-sans bg-zinc-100 dark:bg-zinc-900 text-zinc-700 dark:text-zinc-200 overflow- `}>
 
         {/* (frame) */}
         <section className="w-full h-smscreen md:h-screen md:space-x-2 md:p-2 flex">
---
+++ styles/fonts.ts
@@ -0,0 +1,17 @@
+// import { Inter, Lora, Source_Sans_Pro } from 'next/font/google'
+import localFont from 'next/font/local'
+ 
+// define your variable fonts
+// const inter = Inter()
+// const lora = Lora()
+
+// define 2 weights of a non-variable font
+// const sourceCodePro400 = Source_Sans_Pro({ weight: '400' })
+// const sourceCodePro700 = Source_Sans_Pro({ weight: '700' })
+
+// define a custom local font where GreatVibes-Regular.ttf is stored in the styles folder
+const instrumentSans = localFont({ src: 'InstrumentSans-VariableFont-wdth-wght.ttf' })
+ 
+// export { inter, lora, sourceCodePro400, sourceCodePro700, greatVibes }
+ 
+export { instrumentSans }
